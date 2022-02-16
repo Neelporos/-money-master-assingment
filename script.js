546ee326ba10ada28bde405318657da19c7ba436
Neelporos <sawkotalimahmud@gmail.com>
2022-02-16 12:59:46 -0800

    First Calculation Section Done

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("all ok");
+const calculateButton = document.getElementById('calculate-btn');
+// Calculation Section 
+calculateButton.addEventListener('click', function () {
+    // Error Handling
+
+
+    //   All Cost 
+    const foodCost = document.getElementById('food-cost').value;
+    const rentCost = document.getElementById('rent-cost').value;
+    const clothesCost = document.getElementById('clothes-cost').value;
+
+    const allCost = parseFloat(foodCost) + parseFloat(rentCost) + parseFloat(clothesCost);
+
+    const totalCost = document.getElementById('total-cost')
+    totalCost.innerText = allCost;
+
+    //  Balance Remaining
+    const totalIncome = document.getElementById('total-income').value;
+    const balance = document.getElementById('balance');
+    balance.innerText = totalIncome - allCost;
+})
+
+
+
