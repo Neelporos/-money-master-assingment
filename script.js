6c74993a1472fbb617750cedbb93a94ecaa09063
Neelporos <sawkotalimahmud@gmail.com>
2022-02-17 03:06:56 -0800

    Final Revision

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const calculateButton = document.getElementById('calculate-btn');
 calculateButton.addEventListener('click', function () {
 
+    // Input Section 
     const totalIncome = document.getElementById('total-income').value;
     const foodCost = document.getElementById('food-cost').value;
     const rentCost = document.getElementById('rent-cost').value;
@@ -12,23 +13,21 @@ calculateButton.addEventListener('click', function () {
         return;
     }
 
+    // Calculate Section 
     const allCost = parseFloat(foodCost) + parseFloat(rentCost) + parseFloat(clothesCost);
-
     if (allCost > totalIncome) {
         displayElement("error-massage2")
         return;
     }
-
     const totalCost = document.getElementById('total-cost')
     totalCost.innerText = allCost;
-
     const balance = document.getElementById('balance');
     balance.innerText = totalIncome - allCost;
 
 })
 
 
-// Error Handling Section
+// Error Handling Function
 function displayElement(id) {
     const errorMassage = document.getElementById(id);
     errorMassage.style.display = "block"
@@ -39,17 +38,17 @@ function displayElement(id) {
 const savingButton = document.getElementById("savaing-btn");
 savingButton.addEventListener("click", function () {
 
+    // Input Section 
     const savingInput = document.getElementById("saving-input").value;
     const totalIncome = document.getElementById('total-income').value;
     const totalCost = document.getElementById('total-cost').innerText;
 
+    // Calculate Section
     if (savingInput > 0) {
         const savingAmountId = document.getElementById("saving-amount");
         const savingAmount = totalIncome * (savingInput / 100);
-
-
-
         const remaingBalance = totalIncome - totalCost - savingAmount;
+
         if (remaingBalance < 0) {
             displayElement("error-massage3")
             return;
@@ -57,38 +56,11 @@ savingButton.addEventListener("click", function () {
         savingAmountId.innerText = savingAmount;
         console.log(remaingBalance);
         const remaingBalanceId = document.getElementById('remaing-balance');
-
         remaingBalanceId.innerText = remaingBalance;
     }
+
     else {
         displayElement("error-massage")
     }
 
 })
-
-
-
-
-
-    // if (foodCost > 0 && rentCost > 0 && clothesCost > 0) {
-    //     const allCost = parseFloat(foodCost) + parseFloat(rentCost) + parseFloat(clothesCost);
-
-    //     const totalCost = document.getElementById('total-cost')
-    //     totalCost.innerText = allCost;
-
-
-
-    //     const balance = document.getElementById('balance');
-    //     balance.innerText = totalIncome - allCost;
-    // }
-    // else {
-    //     displayElement("error-massage")
-
-    // }
-
-     // if () {
-    //     displayElement("error-massage2")
-    //     return;
-    // }
-
-     // const totalIncome = document.getElementById('total-income').value;
